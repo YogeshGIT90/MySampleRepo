b7670c7afb92240eebd1e0837fe4c671034af3c4
Yogesh Khot <yogeshkhot113@gmail.com>
2021-12-17 22:45:19 +0530

    initial commit

---
+++ src/resource/Addition.java
@@ -0,0 +1,16 @@
+package resource;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 10;
+		
+		System.out.println(a+b);
+		
+		System.out.print(b);
+		
+	}
+
+}

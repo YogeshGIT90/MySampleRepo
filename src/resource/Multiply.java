53e7dbcd8520b4a696f51796ca9c000c16a0ab8c
YogeshGIT90 <yogeshkhot113@gmail.com>
2021-12-19 15:04:39 +0530

    added multiply java file

---
+++ src/resource/Multiply.java
@@ -0,0 +1,20 @@
+package resource;
+
+public class Multiply {
+
+	
+	int x = 10;
+	int y = 15;
+	
+	public int multiplyvalue(int a, int b) {
+		
+		x =a;
+		y= b;
+		
+		return x*y;
+		
+	}
+			
+			
+	
+}
